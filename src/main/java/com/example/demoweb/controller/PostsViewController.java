9916a702c918263358697a973d36f2f1d4cf514c
nodlya <nadya-popova-02@mail.ru>
2022-09-14 18:55:08 +0500

    list of posts

--- src/main/java/com/example/demoweb/controller/PostsViewController.java
+++ src/main/java/com/example/demoweb/controller/PostsViewController.java
@@ -1,5 +1,7 @@
 package com.example.demoweb.controller;
 
+import com.example.demoweb.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +11,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class PostsViewController {
+
+    @Autowired
+    private PostService postsService = new PostService();
     @ResponseBody
     @RequestMapping(path = "/post/{id}", method = RequestMethod.GET)
     public String single(@PathVariable("id") Long id) {
@@ -18,6 +23,7 @@ public class PostsViewController {
     @RequestMapping(path = "/", method = RequestMethod.GET)
     public String list(Model model) {
         model.addAttribute("appName", "Моё супер приложение))");
+        model.addAttribute("posts",postsService.listAllPosts());
         return "list";
     }
 }
